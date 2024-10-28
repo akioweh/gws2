2b127120778641d0d3dbbf17d69aac0398cf9f68
akioweh <79527273+akioweh@users.noreply.github.com>
2024-10-28 15:18:46 +0000

    add custom process time header middleware

--- webserver/main.py
+++ webserver/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
+
+
+@app.middleware('http')
+async def add_process_time_header(request: Request, call_next):
+    start = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start
+    response.headers['X-Process-Time'] = str(process_time)
+    return response
 
 
 @app.get('/')
@@ -15,5 +26,5 @@ async def favicon():
     return FileResponse('files/favicon.ico')
 
 
-app.mount('/shared/', StaticFiles(directory='files/shared'), name='files')
-app.mount('/apps/', StaticFiles(directory='files/apps'), name='apps')
+app.mount('/shared/', StaticFiles(directory='files/shared', html=True), name='files')
+app.mount('/apps/', StaticFiles(directory='files/apps', html=True), name='apps')
