fe40f84baed07258f1f1f107563c054dca722349
akioweh <79527273+akioweh@users.noreply.github.com>
2024-10-29 12:46:15 +0000

    redirect http to https

--- webserver/main.py
+++ webserver/main.py
@@ -1,6 +1,7 @@
 import time
 
 from fastapi import FastAPI, Request
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 from .staticdir import StaticDir
 
@@ -14,6 +15,10 @@ async def add_process_time_header(request: Request, call_next):
     process_time = time.perf_counter() - start
     response.headers['X-Process-Time'] = str(process_time)
     return response
+
+
+# noinspection PyTypeChecker
+app.add_middleware(HTTPSRedirectMiddleware)
 
 
 app.mount('/', StaticDir(directory='files'), name='root')
