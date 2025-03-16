6d5cf4254e7be987a0b3361f3bf84581df50f1a6
akioweh <79527273+akioweh@users.noreply.github.com>
2025-03-16 08:37:29 +0000

    call encode() once instead of every time

--- webserver/nullrouter.py
+++ webserver/nullrouter.py
@@ -7,7 +7,7 @@ class NullRouter:
     """A FastAPI mount that has a set static response for all endpoints and requests."""
 
     def __init__(self, response: str = ''):
-        self.response = response
+        self.response = response.encode()
 
     async def __call__(self, scope, receive, send):
         await send({
@@ -19,5 +19,5 @@ class NullRouter:
         })
         await send({
             'type': 'http.response.body',
-            'body': self.response.encode(),
+            'body': self.response,
         })
