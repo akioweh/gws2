1f64f5de16e26d5ba6fa14423b3d7ca553822983
akioweh <79527273+akioweh@users.noreply.github.com>
2025-03-18 07:28:15 +0000

    temporarily disable add_process_time middleware because it was breaking with server push test

--- webserver/main.py
+++ webserver/main.py
@@ -22,13 +22,13 @@ def create_app(redirect_insecure: bool = True) -> FastAPI:
         # noinspection PyTypeChecker
         app.add_middleware(HTTPSRedirectMiddleware)
 
-    @app.middleware('http')
-    async def add_process_time_header(request: Request, call_next: RequestResponseEndpoint) -> Response:
-        start = time.perf_counter()
-        response = await call_next(request)
-        process_time = time.perf_counter() - start
-        response.headers['X-Process-Time'] = str(process_time)
-        return response
+    # @app.middleware('http')
+    # async def add_process_time_header(request: Request, call_next: RequestResponseEndpoint) -> Response:
+    #     start = time.perf_counter()
+    #     response = await call_next(request)
+    #     process_time = time.perf_counter() - start
+    #     response.headers['X-Process-Time'] = str(process_time)
+    #     return response
 
     app.mount('/', StaticDir(directory='files'), name='root')
 
